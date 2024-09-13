3621506fb1090792f72971ae99ccafeac106cb0f
Rommel <alexanderommelsw@outlook.es>
2024-09-13 17:36:05 -0500

    Create UserTestData.kt

---
+++ testing/src/main/java/com/alexandersw/testing/UserTestData.kt
@@ -0,0 +1,8 @@
+package com.alexandersw.testing
+
+import com.alexandersw.api.user.User
+
+class UserTestData {
+}
+
+val test_data_user1 = User(id = "1", firstname = "Alexander", email = "alexanderommel@gmail.com", accessToken = "token")
