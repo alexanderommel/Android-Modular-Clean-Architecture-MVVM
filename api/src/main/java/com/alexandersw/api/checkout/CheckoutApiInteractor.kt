717906b852c0fcc560b60748523345ed30e855f1
Rommel <alexanderommelsw@outlook.es>
2024-09-13 17:38:07 -0500

    Update CheckoutApiInteractor.kt

--- api/src/main/java/com/alexandersw/api/checkout/CheckoutApiInteractor.kt
+++ api/src/main/java/com/alexandersw/api/checkout/CheckoutApiInteractor.kt
@@ -1,11 +1,10 @@
 package com.alexandersw.api.checkout
 
 import com.alexandersw.api.dto.LineItemDto
-import com.alexandersw.api.dto.ShoppingCartDto
 import com.alexandersw.common.UseCaseResponse
 import kotlinx.coroutines.flow.Flow
 
 interface CheckoutApiInteractor {
     suspend fun addLineItem(lineItem: LineItemDto, storeId: Int): UseCaseResponse<Boolean>
-    fun getMyShoppingCarts(): Flow<List<ShoppingCartDto>>
+    suspend fun getAll(): Flow<List<Checkout>>
 }
