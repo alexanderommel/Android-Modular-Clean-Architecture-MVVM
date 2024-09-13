07f6829bfc48c2892c108923afaec9c23139ab4c
Rommel <alexanderommelsw@outlook.es>
2024-09-13 17:38:17 -0500

    Update StoreRemoteRepository.kt

--- api/src/main/java/com/alexandersw/api/stores/StoreRemoteRepository.kt
+++ api/src/main/java/com/alexandersw/api/stores/StoreRemoteRepository.kt
@@ -6,4 +6,6 @@ import com.alexandersw.common.UseCaseResponse
 interface StoreRemoteRepository {
     suspend fun getStores(near: Location): UseCaseResponse<List<Store>>
     suspend fun getCatalogue(storeId: Int): UseCaseResponse<Catalogue>
+    suspend fun getStore(id: Int, bearerToken: String): UseCaseResponse<Store>
+    suspend fun getProduct(id: Int, bearerToken: String): UseCaseResponse<Product>
 }
