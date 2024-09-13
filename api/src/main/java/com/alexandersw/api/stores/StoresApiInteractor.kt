a81c3675eae4a98f5911f3e4e45ecfb066ca3d8d
Rommel <alexanderommelsw@outlook.es>
2024-09-13 17:38:13 -0500

    Update StoresApiInteractor.kt

--- api/src/main/java/com/alexandersw/api/stores/StoresApiInteractor.kt
+++ api/src/main/java/com/alexandersw/api/stores/StoresApiInteractor.kt
@@ -6,4 +6,6 @@ interface StoresApiInteractor {
     // The location is retrieved from the user stored data
     suspend fun getStores(): UseCaseResponse<List<Store>>
     suspend fun getCatalogue(from: Store): UseCaseResponse<Catalogue>
+    suspend fun getStoreById(id: Int): UseCaseResponse<Store>
+    suspend fun getProductById(id: Int): UseCaseResponse<Product>
 }
