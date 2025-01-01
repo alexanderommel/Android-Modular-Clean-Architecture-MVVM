d01af5a8972ef0d7664b7cb2d672ba8a81e2c2da
Rommel <alexanderommelsw@outlook.es>
2025-01-01 01:01:39 -0500

    Create StoreRemoteRepositoryImpl.kt

---
+++ network/src/main/java/com/alexandersw/network/stores/StoreRemoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.alexandersw.network.stores
+
+import com.alexandersw.api.stores.Catalogue
+import com.alexandersw.api.stores.Product
+import com.alexandersw.api.stores.Store
+import com.alexandersw.api.stores.StoreRemoteRepository
+import com.alexandersw.common.Location
+import com.alexandersw.common.UseCaseResponse
+
+class StoreRemoteRepositoryImpl{ /**StoreRemoteRepository {
+    override suspend fun getStores(near: Location): UseCaseResponse<List<Store>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCatalogue(storeId: Int): UseCaseResponse<Catalogue> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getStore(id: Int, bearerToken: String): UseCaseResponse<Store> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getProduct(id: Int, bearerToken: String): UseCaseResponse<Product> {
+        TODO("Not yet implemented")
+    }**/
+}
