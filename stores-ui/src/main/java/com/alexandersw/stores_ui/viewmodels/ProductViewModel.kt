9a9ef5906c33c84b34ce7b3801a36b98c4f34b81
Rommel <alexanderommelsw@outlook.es>
2025-01-01 01:02:04 -0500

    Update ProductViewModel.kt

--- stores-ui/src/main/java/com/alexandersw/stores_ui/viewmodels/ProductViewModel.kt
+++ stores-ui/src/main/java/com/alexandersw/stores_ui/viewmodels/ProductViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.alexandersw.api.checkout.CheckoutApiInteractor
 import com.alexandersw.api.dto.LineItemDto
+import com.alexandersw.api.stores.Product
+import com.alexandersw.api.stores.StoresApiInteractor
 import com.alexandersw.common.UseCaseResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -13,7 +16,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProductViewModel @Inject constructor(
-    private val api: CheckoutApiInteractor
+    private val api: CheckoutApiInteractor,
+    private val storesApi: StoresApiInteractor
 ) : ViewModel() {
 
     // StateFlow to manage the error message
@@ -27,6 +31,28 @@ class ProductViewModel @Inject constructor(
     // StateFlow to manage whether the item has been added
     private val _itemAdded = MutableStateFlow(false)
     val itemAdded: StateFlow<Boolean> get() = _itemAdded
+
+    private val _productState = MutableStateFlow<Product?>(null)
+    val productState: StateFlow<Product?> = _productState
+
+    fun loadProduct(productId: Int){
+        viewModelScope.launch(Dispatchers.IO) {
+
+            _isLoading.value = true
+
+            val response = storesApi.getProductById(productId)
+            when (response) {
+                is UseCaseResponse.Error -> {
+                    _errorLocalized.value = response.failure.handleBusinessRuleFailure()
+                    _isLoading.value = false
+                }
+                is UseCaseResponse.Success -> {
+                    _isLoading.value = false
+                    _productState.value = response.data
+                }
+            }
+        }
+    }
 
     // Function to add a line item to the shopping cart
     fun addLineItemToShoppingCart(storeId: Int, lineItem: LineItemDto) {
