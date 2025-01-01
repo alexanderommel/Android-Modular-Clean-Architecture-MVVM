65c64fa73eb28762cecaea55e6d048095c33f231
Rommel <alexanderommelsw@outlook.es>
2025-01-01 01:01:29 -0500

    Update AppNavigation.kt

--- app/src/main/java/com/alexandersw/dandelion/AppNavigation.kt
+++ app/src/main/java/com/alexandersw/dandelion/AppNavigation.kt
@@ -2,6 +2,7 @@ package com.alexandersw.dandelion
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,7 +10,9 @@ import androidx.navigation.compose.rememberNavController
 import com.alexandersw.api.stores.Catalogue
 import com.alexandersw.api.stores.Store
 import com.alexandersw.stores_ui.screens.CatalogueScreen
+import com.alexandersw.stores_ui.screens.ProductDetailScreen
 import com.alexandersw.stores_ui.screens.StoresScreen
+import com.alexandersw.stores_ui.viewmodels.StoresViewModel
 import com.alexandersw.testing.test_data_user1
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -17,20 +20,24 @@ import com.google.gson.reflect.TypeToken
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
+    val storesViewModel: StoresViewModel = hiltViewModel()
 
     NavHost(
         navController = navController,
         startDestination = "storesScreen"
     ) {
+
+
+
         composable("storesScreen") {
-            StoresScreen(navController = navController,user = test_data_user1)
+            StoresScreen(navController = navController,user = test_data_user1, storesViewModel = storesViewModel)
         }
         composable("catalogueScreen") { backStackEntry ->
-            val storeJson = backStackEntry.arguments?.getString("store")!!
-            //val type = object: TypeToken<Catalogue>() {}.type
-            val s = Gson().fromJson(storeJson, Store::class.java)!!
-            CatalogueScreen(store = s, navController = navController)
+            CatalogueScreen(navController = navController, storesViewModel = storesViewModel)
         }
-        //composable("productScreen") { DetailScreen(navController) }
+        composable("productScreen/{productId}") { backStackEntry ->
+            val productId = backStackEntry.arguments?.getString("productId")?.toInt()
+            ProductDetailScreen(navController,productId = productId ?: 0, storesViewModel = storesViewModel)
+        }
     }
 }
